22f1c0d09ae632c2ae2f840abeeb265f39dbdcdc
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2024-01-12 22:55:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from openai import OpenAI
 # Chuyển khóa API cho đối tượng OpenAI client
 client = OpenAI(api_key="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")
 
-
 def generate_response(prompt):
     response = client.completions.create(
         model="davinci-002", 
