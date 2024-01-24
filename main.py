100f04539e33ec1ebfaee697ef9f360d8263bf13
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2024-01-24 19:56:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ st.set_page_config(
     page_icon=":robot:"
 )
 st.header("ChatGPT Clone with Streamlit")
-
+nsdbfjsdbfsdjlfbsdJFBDSJFBDSJFBDS
 history_input = []
 
 if 'generated' not in st.session_state:
